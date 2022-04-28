012bb41700490a0ff678deb5daea41e88e9422cc
imranshuvo <imrankhanshuvo@gmail.com>
2022-04-29 04:00:45 +0600

    Added loader

--- script.js
+++ script.js
@@ -3,11 +3,25 @@ const quoteText = document.getElementById('quote');
 const authorText = document.getElementById('author');
 const twitterBtn = document.getElementById('twitter');
 const newQuoteBtn = document.getElementById('new-quote');
+const loader = document.getElementById('loader');
 
 let apiQuotes = [];
 
+// Show loading
+function loading() {
+    loader.hidden = false;
+    quoteContainer.hidden = true;
+}
+// Close loading
+function complete() {
+    loader.hidden = true;
+    quoteContainer.hidden = false;
+}
+
 // Show New Quote
 function newQuote() {
+    // Call the loader
+    loading();
     // Pick a random quote from apiQuotes array
     const quote = apiQuotes[Math.floor(Math.random() * apiQuotes.length)];
     // Check for long text
@@ -15,10 +29,14 @@ function newQuote() {
     quoteText.textContent = quote.text;
     // Only populate author if it's not blank
     authorText.textContent = !quote.author ? 'Unknown' : quote.author ;
+    // complete; hide the loader
+    complete();
 }
 
 // Get Quotes From API
 async function getQuotes() {
+    // Call the loader
+    loading();
     const apiUrl = 'https://type.fit/api/quotes';
     try {
         const response = await fetch(apiUrl);
